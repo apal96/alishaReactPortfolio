72413cba3dbb23f05c0eedf336cc07108c552162
apal <alishapal96@gmail.com>
2021-08-25 22:20:27 -0700

    add pages and navbar

---
+++ src/components/NavTabs.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+         
+
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#blog"
+          onClick={() => handlePageChange('Work')}
+        
+
+          className={currentPage === 'Work' ? 'nav-link active' : 'nav-link'}
+        >
+          Blog
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+       
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
---
+++ src/components/Portfolio.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import About from './pages/About';
+import Blog from './pages/Work';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Blog />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+ 
+      {renderPage()}
+    </div>
+  );
+}
