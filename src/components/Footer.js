0b24f976590f975aea58a980a6820e3c831d6b7a
apal <alishapal96@gmail.com>
2021-08-26 00:36:32 -0700

    footer link styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,26 @@
 import React from 'react';
 // // Here we are importing a CSS file as a dependency
 // import '../styles/Header.css';
+import '../components/styles/Footer.css';
 
 function Footer() {
   return (
-    <header className="header">
-      <h1>Alisha Pal</h1>
-    </header>
+
+    <footer class = "footer-nav">  
+       
+
+     <ul class="nav justify-content-center">
+ 
+       <li class="nav-item">
+         <a class="nav-link" href="https://www.linkedin.com/in/alisha-pal-6635361b5/" target="_blank" rel="noreferrer" id ="linked-in">LinkedIn</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="https://github.com/apal96" target="_blank" rel="noreferrer"  id ="github">Github</a>
+       </li>
+
+     </ul>
+    
+     </footer>
   );
 }
 
